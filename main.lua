ce5ec8ba09a9d257c24ae02f1d9ba20b87ecf793
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-03-29 05:24:59 -0400

    MIMU V1 (gabriel morreu................)

--- main.lua
+++ main.lua
@@ -1,27 +1,24 @@
-local MyCharacterMod = RegisterMod("Gabriel Character Mod", 1)
-local mimuType = Isaac.GetPlayerTypeByName("Gabriel", false) -- Exactly as in the xml. The second argument is if you want the Tainted variant.
--- Exact paths, with the "resources" folder as the root
+local MimuMod = RegisterMod("Mimu Character Mod", 1)
 local hairCostume = Isaac.GetCostumeIdByPath("gfx/characters/mimu_hair.anm2")
 local hairCostume2 = Isaac.GetCostumeIdByPath("gfx/characters/felix_hair.anm2")
 local hairCostume3 = Isaac.GetCostumeIdByPath("gfx/characters/niya_hair.anm2")
-local stolesCostume = Isaac.GetCostumeIdByPath("gfx/characters/mimu_stoles.anm2")
  
-function MyCharacterMod:GiveCostumesOnInit(player)
-    if player:GetPlayerType() ~= mimuType then
-        return -- End the function early. The below code doesn't run, as long as the player isn't Gabriel.
+function MimuMod:GiveCostumesOnInit(player)
+    if player:GetPlayerType() ~= Isaac.GetPlayerTypeByName("Mimu", false) then
+        return
     end
 
     player:AddNullCostume(hairCostume)
-    player:AddNullCostume(stolesCostume)
 end
-MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.GiveCostumesOnInit)
+MimuMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MimuMod.GiveCostumesOnInit)
 
-function MyCharacterMod:ExtraCostumesOnInit(player)
+function MimuMod:ExtraCostumesOnInit(player)
     if WardrobePlus ~= nil then
         OWRP.AddNewCostume("mimuhair", "Mimu's hair", "gfx/characters/mimu_hair.anm2",false,false)
         OWRP.AddNewCostume("felihair", "Felix's hair", "gfx/characters/felix_hair.anm2",false,false)
         OWRP.AddNewCostume("niyahair", "Niya's hair", "gfx/characters/niya_hair.anm2",false,false)
         OWRP.AddNewCostume("tobyhair", "Toby's hair", "gfx/characters/toby_hair.anm2",false,false)
+        OWRP.AddNewCostume("clarhair", "Clara's hair", "gfx/characters/clara_hair.anm2",false,false)
     end
 end
-MyCharacterMod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, MyCharacterMod.ExtraCostumesOnInit)
+MimuMod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, MimuMod.ExtraCostumesOnInit)
