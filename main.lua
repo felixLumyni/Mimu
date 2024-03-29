147ce1f14dc61c66c2096069f08b4df3c538e04b
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-03-29 18:17:26 -0400

    oops

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ function MimuMod:PostPlayerInit(player)
         return
     end
 
-    player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/mimu_hair.anm2"))
+    player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/mimu_hair_character.anm2"))
     player:AddEternalHearts(1)
 end
 MimuMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MimuMod.PostPlayerInit)
@@ -20,7 +20,9 @@ function MimuMod:HandleStartingStats(player, flag)
         player.Damage = player.Damage * 2
     end
     if flag == CacheFlag.CACHE_FIREDELAY then
-        player.FireDelay = player.FireDelay / 1.75
+        player.MaxFireDelay = player.MaxFireDelay * 1.75
+    end
+    if flag == CacheFlag.CACHE_SHOTSPEED then
         player.ShotSpeed = player.ShotSpeed / 3
     end
 end
