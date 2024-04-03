6d414a12b769d3af5e6db4cb9f591bcdaef77437
Nininiya69 <126848002+Nininiya69@users.noreply.github.com>
2024-04-03 07:21:26 -0400

    trying to put the item in the game but failing
    - tried to make png working, but even the item dont exist ingame. help felix

--- main.lua
+++ main.lua
@@ -1,32 +1,14 @@
 local MimuMod = RegisterMod("Mimu Character Mod", 1)
 local MimuChar = Isaac.GetPlayerTypeByName("Mimu", false)
+local HomuosWater = Isaac.GetItemIdByName("Homuo's Water")
 
 --Add trinkets and items into pools
 TrinketType.TRINKET_AXSHAPEDLEAF = Isaac.GetTrinketIdByName("Ax-Shaped Leaf")
 CollectibleType.COLLECTIBLE_HOMUOSWATER = Isaac.GetItemIdByName("Homuo's Water")
 
 --Functions & Callbacks
-function MimuMod:onNewFloor()
-    local player = Isaac.GetPlayer(0)
-        if player:HasCollectible(Isaac.GetItemIdByName("Homuo's Water")) then
-        player:AddEternalHearts(1, false)
-    end
-end
-MimuMod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, MimuMod.onNewFloor)
 
-function MimuMod:PostPlayerInit(player)
-    player:GetData().mimuCooldown = 0
-    if player:GetPlayerType() ~= MimuChar then
-        return
-    end
-
-    player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/mimu_hair_character.anm2"))
-    player:AddEternalHearts(1)
-    player:AddTrinket(TrinketType.TRINKET_AXSHAPEDLEAF)
-    player:AddCollectible(CollectibleType.COLLECTIBLE_HOMUOSWATER, 0, true, ActiveSlot.SLOT_PRIMARY, 0)
-end
-MimuMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MimuMod.PostPlayerInit)
-
+--Mimu's base stats
 function MimuMod:HandleStartingStats(player, flag)
     if player:GetPlayerType() ~= MimuChar then
         return
@@ -44,6 +26,31 @@ function MimuMod:HandleStartingStats(player, flag)
 end
 MimuMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MimuMod.HandleStartingStats)
 
+--Mimu start game
+function MimuMod:PostPlayerInit(player)
+    player:GetData().mimuCooldown = 0
+    if player:GetPlayerType() ~= MimuChar then
+        return
+    end
+
+    player:AddNullCostume(Isaac.GetCostumeIdByPath("gfx/characters/mimu_hair_character.anm2"))
+    player:AddEternalHearts(1)
+    player:AddTrinket(TrinketType.TRINKET_AXSHAPEDLEAF)
+    player:AddCollectible(CollectibleType.COLLECTIBLE_HOMUOSWATER, 0, false)
+end
+MimuMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MimuMod.PostPlayerInit)
+
+--Homuo's Water
+function MimuMod:onNewFloor()
+    local player = Isaac.GetPlayer(0)
+        if player:HasCollectible(HomuosWater) then
+        player:AddEternalHearts(1, false)
+        player.Damage = player.Damage + 0.5
+    end
+end
+MimuMod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, MimuMod.onNewFloor)
+
+--custumes 
 function MimuMod:ExtraCostumesOnInit(player)
     if WardrobePlus ~= nil then
         OWRP.AddNewCostume("mimuhair", "Mimu's hair", "gfx/characters/mimu_hair.anm2",false,false)
@@ -55,6 +62,7 @@ function MimuMod:ExtraCostumesOnInit(player)
 end
 MimuMod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, MimuMod.ExtraCostumesOnInit)
 
+--trinket ax-shaped leaf
 function MimuMod:onDamage(entity, damageAmount, damageFlags, damageSource, damageCountdown)
     local enemyOnFullHP = entity:IsActiveEnemy() and entity.HitPoints == entity.MaxHitPoints
     if enemyOnFullHP then
@@ -74,6 +82,7 @@ function MimuMod:onDamage(entity, damageAmount, damageFlags, damageSource, damag
 end
 MimuMod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, MimuMod.onDamage)
 
+--q porra essa kk
 function MimuMod:onNewRoom()
     for i = 1, Game():GetNumPlayers() do
         local player = Game():GetPlayer(i - 1)
